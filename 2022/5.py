a90a7d040810afa0e5332eecbf2f825a8cda6e26
elena <elenajeanday@gmail.com>
2024-11-03 15:41:35 +0000

    added d5p2

--- 2022/5.py
+++ 2022/5.py
@@ -50,52 +50,42 @@ The Elves just need to know which crate will end up on top of each stack; in thi
 so you should combine these together and give the Elves the message CMZ.
 
 After the rearrangement procedure completes, what crate ends up on top of each stack?
-
+""" 
+"""
 --- Part Two ---
-As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
-
-Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
-
-The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+The CrateMover 9001 is notable for the ability to pick up and move multiple crates at once.
 
 Again considering the example above, the crates begin in the same configuration:
-
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+        [D]    
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
 Moving a single crate from stack 2 to stack 1 behaves the same as before:
-
-[D]        
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+    [D]        
+    [N] [C]    
+    [Z] [M] [P]
+    1   2   3 
 However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
-
-        [D]
-        [N]
-    [C] [Z]
-    [M] [P]
- 1   2   3
+            [D]
+            [N]
+        [C] [Z]
+        [M] [P]
+    1   2   3
 Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
-
-        [D]
-        [N]
-[C]     [Z]
-[M]     [P]
- 1   2   3
+            [D]
+            [N]
+    [C]     [Z]
+    [M]     [P]
+    1   2   3
 Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
-
-        [D]
-        [N]
-        [Z]
-[M] [C] [P]
- 1   2   3
+            [D]
+            [N]
+            [Z]
+    [M] [C] [P]
+    1   2   3
 In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
 
-Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
-
-
+After the rearrangement procedure completes, what crate ends up on top of each stack?
 """
 
 import sys
@@ -133,7 +123,7 @@ def get_start() -> list[list, list]:
         moves.append([int(step[1]), int(step[3])-1, int(step[5])-1]) 
     return [reversed_line, moves]
 
-def follow_step(crates: list, step: list) -> list[list]:
+def move_one_by_one(crates: list, step: list) -> list[list]:
     crates_to_move, from_stack, to_stack = step
     for i in range(crates_to_move):
         crate_val = crates[from_stack][-1]
@@ -141,16 +131,28 @@ def follow_step(crates: list, step: list) -> list[list]:
         crates[from_stack] = crates[from_stack][0:-1]
     return crates
 
+def move_together(crates: list, step: list) -> list[list]:
+    crates_to_move, from_stack, to_stack = step
+    crates_to_move = 0-crates_to_move
+    crate_val = crates[from_stack][crates_to_move:]
+    crates[to_stack]+=crate_val
+    crates[from_stack] = crates[from_stack][0:crates_to_move]
+    return crates
+
 def part_one():
     crates, steps = get_start()
     for step in steps:
-        crates = follow_step(crates, step)
+        crates = move_one_by_one(crates, step)
     print(''.join([c[-1] for c in crates]))
     return
 
 def part_two():
+    crates, steps = get_start()
+    for step in steps:
+        crates = move_together(crates, step)
+    print(''.join([c[-1] for c in crates]))
     return
 
 input = aoc_utils.read_file('data/d5.txt')
 part_one() # HNSNMTLHQ
-part_two()
+part_two() # RNLFDJMCT
